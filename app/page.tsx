f0c19dcd1d447c0fde4e142bed3ea817f58a2f70
ogamanbs <134797606+ogamanbs@users.noreply.github.com>
2023-09-22 16:17:52 +0530

    added stay tune...

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className='main'>
-      <h1>Ripple</h1>
-      <h3>Events and Tours</h3>
+      <div className="element">
+        <div className="logo">
+          <img src="favicon.ico" alt="" />
+        </div>
+        <div className='name'>
+          <h1>Ripple</h1>
+          <h3>Events and Tours</h3>
+        </div>
+      </div>
+      <div className="staytune">
+        <h1>Stay Tune...</h1>
+      </div>
     </div>
   )
 }
